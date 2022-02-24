e7a7440b136634e5b2fe58900c00324b0f0af149
Miguel Santacruz <miguel.santacruz@mths.ca>
2022-02-24 19:48:00 +0000

    area_and_perimeter

---
+++ Math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Miguel Santacruz
+# Created on: February 2022
+# This program does basic math
+
+
+def main():
+    #this function does basic math
+
+    print(9 + 2)
+    print("7 - 3 = {}".format(7-3))
+    print("5 + 2^3 = {}".format(5+2**3))
+
+
+if __name__ == "__main__":
+    main()
---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Miguel Santacruz
+# Created on: February 2022
+# This is the "Hello, world!" program, with proper style
+
+
+def main():
+    #This function calculates area and perimeter
+
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Its area is {}cm^2".format(5*3))
+    print("Its perimeter is {}cm".format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
