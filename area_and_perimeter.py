296ea9bd902938804894344177912527c76e3013
Miguel Santacruz <miguel.santacruz@mths.ca>
2022-02-28 04:57:00 +0000

    area and perimeter

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -6,13 +6,13 @@
 
 
 def main():
-    #This function calculates area and perimeter
+    # This function calculates area and perimeter
 
     print("If a rectangle has the dimensions:")
     print("5cm x 3cm")
     print("")
-    print("Its area is {}cm^2".format(5*3))
-    print("Its perimeter is {}cm".format(2*(5+3)))
+    print("Its area is {}cm^2".format(5 * 3))
+    print("Its perimeter is {}cm".format(2 * (5 + 3)))
 
 
 if __name__ == "__main__":
